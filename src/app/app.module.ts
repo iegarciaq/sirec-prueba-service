2dadd6309097c83b387b1d5ca8d857069eab629e
IvanGQ <ivan.garcia@utxicotepec.edu.mx>
2022-07-03 09:33:13 -0500

    Carga inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
